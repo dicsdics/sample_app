d961fc9d970724de41f5406a7ff9d59196ced41d
Anton <anton_megabox@mail.ru>
2014-08-28 20:50:09 +0300

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+# adding this fixes the issue
+require 'tzinfo'
+
+# Initialize the rails application
+Rails.application.initialize!
+
+KEY_1 = "aghtbjki"
+SHARED_SECRET_1 = "plkjuvfe"
+KEY_2 = "ljfebjyf"
